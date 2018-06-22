c8ff9803399b2292ce86cfc1bf66487039f4ff1d
aalok.sathe <aalok.sathe@gmail.com>
2018-06-22 15:27:56 -0400

    almost finish writing preparation methods 	modified:   cfd-reader/preparation.py

--- cfd-reader/preparation.py
+++ cfd-reader/preparation.py
@@ -17,10 +17,11 @@ from . import faces
 def is_prepared(home=None, inst=None) -> bool:
     """Checks to see if the installation directory exists and is well"""
     bool = os.path.isdir(inst)
+    cached = os.path.isdir(os.path.join(inst, 'images'))
     for file in {'cfd_stats.csv','images.json'}:
         bool &= os.path.isfile(os.path.join(inst, file))
     if not bool: print ("Installation is pending, broken, or incomplete.")
-    return bool
+    return bool, cached
 
 def prepare(home=None, inst=None, cfddir=None) -> None:
     """Prepares installation directory, processes spreadsheet data, and
@@ -28,8 +29,10 @@ def prepare(home=None, inst=None, cfddir=None) -> None:
     # make installation directory in user's home
     try:
         os.mkdir(inst)
+        os.mkdir(os.path.join(inst,'images'))
     except PermissionError:
         print("Error, could not make directory", inst,": insufficient access")
+        raise SystemExit
 
     # retrieve user's downloaded copy of the Chicago Face DB
     if cfddir == None:
@@ -44,14 +47,49 @@ def prepare(home=None, inst=None, cfddir=None) -> None:
         excelsh = list(filter(lambda x: x.endswith('.xlsx'), cfddirls))
     except FileNotFoundError:
         print("Error finding and accessing supplied directory at", cfddir)
+        raise SystemExit
 
     # evaluate validity of expected files in the directory
     bool &= len(imgsdir) == 1 & len(excelsh) == 1
-
     # assert condition that all checks are met, exit with error message if not
     assert bool, "Invalid path supplied. Please re-run and supply correct path"\
                + "\nThe path must directly contain the image directory, and "\
                + "the data spreadsheet. If you haven't, please download the "\
                + "Chicago Face Database and extract it to a convenient location"
+    imgsdir = imgsdir[0]
+    excelsh = excelsh[0]
 
-    img_containers = [os.path.join(imgsdir, x) for x in os.listdir(imgsdir)]
+    # process xlsx file and extract first sheet into instdir
+    try:
+        excelsh = xlrd.open_workbook(excelsh)
+    except FileNotFoundError:
+        print("Error, could not open CFD face data spreadsheet")
+        raise SystemExit
+    data_sheet = excelsh.sheet_by_index(0)
+    # use the csv module to output into csv
+    with open(os.path.join(inst,'data.csv'), 'w') as csvout:
+        csvwriter = csv.writer(csvout, quoting=csv.QUOTE_ALL)
+        for row in range(data_sheet.nrows):
+            csvwriter.writerow(data_sheet.row_values(row))
+
+    img_containers = [os.path.join(imgsdir, name) for name in os.listdir(imgsdir) if '-' in name]
+
+    # gather options for index_faces operation
+    print("Beginning to index faces.")
+    crop_square = True
+    if input("Crop the faces square? (y/n) ").lower() == 'n':
+        crop_square = False
+    cache = True
+    if input("Cache image files in install directory? (y/n): ").lower() == 'n':
+        cache = False
+    resize = input("Resize images to custom dimentions? Input 'height width',"
+                 + " e.g. (default): 32 32. 'n' for NO.\n").lower()
+    if resize == 'n': resize = None
+    elif not len(resize): resize = (32,32)
+    else: resize = tuple(resize.split())
+
+    (img_ref_dict,
+    indexed_faces) = faces.index_faces(imgsdir=imgsdir, inst=inst,
+                                       img_containers=img_containers,
+                                       cache=cache, resize=resize,
+                                       crop_square=crop_square)
