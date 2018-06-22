3e02f3cb699a2cf436da9522de899a7c4e10992d
aalok.sathe <aalok.sathe@gmail.com>
2018-06-22 11:53:24 -0400

    name global vars and call preparation methods in init file 	modified:   cfd-reader/__init__.py

--- cfd-reader/__init__.py
+++ cfd-reader/__init__.py
@@ -1,6 +1,16 @@
 from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
+# from __future__ import print_function
+# from __future__ import division
+
+import os
 
 from . import preparation
 from . import faces
+
+global home = os.path.expanduser('~')
+global inst = os.path.join(home, ".cfd-faces")
+
+try:
+    assert preparation.is_prepared(home=home, inst=inst)
+except AssertionError:
+    preparation.prepare(home=home, inst=inst)
