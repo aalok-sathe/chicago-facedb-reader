5d2d09a7e9efcfe24161233354458c2b5f8639df
aalok.sathe <aalok.sathe@gmail.com>
2018-06-22 11:55:15 -0400

    begin writing methods to check for installation files 	modified:   cfd-reader/preparation.py

--- cfd-reader/preparation.py
+++ cfd-reader/preparation.py
@@ -4,32 +4,54 @@ from __future__ import absolute_import
 # from __future__ import print_function
 # from __future__ import division
 
-import os
-import cv2
-import typing
-import json
+import os # for filesystem access
+import cv2 # opencv library for image processing
+import typing # for function annotations
+import json # to dump objects in the json format (as opposed to pickled binary)
+import xlrd,csv # to read the CFD data codebook and output it to a csv file
+from progressbar import progressbar # to display progress during iteration
+from collections import defaultdict # to use during indexing faces
 
-global _home = os.path.expanduser('~')
-global _inst = os.path.join(_home, ".cfd-faces")
+from . import faces
 
-def is_prepared() -> bool:
+def is_prepared(home=None, inst=None) -> bool:
     """Checks to see if the installation directory exists and is well"""
-    bool = os.path.isdir(_inst)
+    bool = os.path.isdir(inst)
     for file in {'cfd_stats.csv','images.json'}:
-        bool &= os.path.isfile(os.path.join(_inst, file))
+        bool &= os.path.isfile(os.path.join(inst, file))
     if not bool: print ("Installation is pending, broken, or incomplete.")
     return bool
 
-def prepare() -> :
-    os.mkdir(_inst)
-    cfddir = input("Please input the full path to the CFD directory that "
-        + "directly contains the images directory and the data spreadsheet.\n")
+def prepare(home=None, inst=None, cfddir=None) -> None:
+    """Prepares installation directory, processes spreadsheet data, and
+       indexes faces in the CFD images directory and dumps them to .json"""
+    # make installation directory in user's home
+    try:
+        os.mkdir(inst)
+    except PermissionError:
+        print("Error, could not make directory", inst,": insufficient access")
+
+    # retrieve user's downloaded copy of the Chicago Face DB
+    if cfddir == None:
+        cfddir = input("Please input the full path to the CFD directory that "
+         + "directly contains the images directory and the data spreadsheet.\n")
     bool = os.path.isdir(cfddir)
-    cfddirls = [os.path.join(cfddir, x) for x in os.listdir(cfddir)]
-    imgsdir = list(filter(lambda x: os.path.isdir(x), cfddirls))
-    excelsh = list(filter(lambda x: x.endswith('.xlsx'), cfddirls))
+
+    # see if the user has provided a correct dir that is properly structured
+    try:
+        cfddirls = [os.path.join(cfddir, x) for x in os.listdir(cfddir)]
+        imgsdir = list(filter(lambda x: os.path.isdir(x), cfddirls))
+        excelsh = list(filter(lambda x: x.endswith('.xlsx'), cfddirls))
+    except FileNotFoundError:
+        print("Error finding and accessing supplied directory at", cfddir)
+
+    # evaluate validity of expected files in the directory
     bool &= len(imgsdir) == 1 & len(excelsh) == 1
+
+    # assert condition that all checks are met, exit with error message if not
     assert bool, "Invalid path supplied. Please re-run and supply correct path"\
                + "\nThe path must directly contain the image directory, and "\
                + "the data spreadsheet. If you haven't, please download the "\
                + "Chicago Face Database and extract it to a convenient location"
+
+    img_containers = [os.path.join(imgsdir, x) for x in os.listdir(imgsdir)]
